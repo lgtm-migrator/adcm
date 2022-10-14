98677e7e32801b4d621dd0c3d9f11cb0f10ea044
Sofya Talavrinova <tsd@arenadata.io>
2022-10-14 11:56:42 +0300

    Fix trailing whitespaces in create licence, refactor licence_checker

--- licence_checker.py
+++ licence_checker.py
@@ -14,61 +14,85 @@ import sys
 import os
 import argparse
 from pathlib import Path
+from typing import TextIO
 
-APACHE_LICENCE = ['Licensed under the Apache License, Version 2.0 (the "License");',
-                  "you may not use this file except in compliance with the License.",
-                  "You may obtain a copy of the License at",
-                  "",
-                  "     http://www.apache.org/licenses/LICENSE-2.0",
-                  "",
-                  "Unless required by applicable law or agreed to in writing, software",
-                  'distributed under the License is distributed on an "AS IS" BASIS,',
-                  "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.",
-                  "See the License for the specific language governing permissions and",
-                  "limitations under the License.",
-                  "\n"]
+APACHE_LICENCE_PY = [
+    '# Licensed under the Apache License, Version 2.0 (the "License");\n',
+    '# you may not use this file except in compliance with the License.\n',
+    '# You may obtain a copy of the License at\n',
+    '#\n',
+    '#      http://www.apache.org/licenses/LICENSE-2.0\n',
+    '#\n',
+    '# Unless required by applicable law or agreed to in writing, software\n',
+    '# distributed under the License is distributed on an "AS IS" BASIS,\n',
+    '# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n',
+    '# See the License for the specific language governing permissions and\n',
+    '# limitations under the License.\n',
+    '\n'
+]
+
+APACHE_LICENCE_GO = [
+    '// Licensed under the Apache License, Version 2.0 (the "License");\n',
+    '// you may not use this file except in compliance with the License.\n',
+    '// You may obtain a copy of the License at\n',
+    '//\n',
+    '//      http://www.apache.org/licenses/LICENSE-2.0\n',
+    '//\n',
+    '// Unless required by applicable law or agreed to in writing, software\n',
+    '// distributed under the License is distributed on an "AS IS" BASIS,\n',
+    '// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n',
+    '// See the License for the specific language governing permissions and\n',
+    '// limitations under the License.\n',
+    '\n'
+]
 
 
-def check_licence(file, fix) -> bool:
+def check_licence(file: str, fix: bool, licence: list[str]) -> bool:
     """Return False in case of empty licence"""
     try:
         with open(file, "r+", encoding="utf-8") as f:
             lines = f.readlines()
+            if len(lines) < 10:
+                if fix:
+                    update_files(f, lines, licence)
+                return False
+            if (lines[0] == licence[0]) and (lines[10] == licence[10]) \
+                    or (lines[1] == licence[0]) and (lines[11] == licence[10]):
+                return True
+            if fix:
+                update_files(f, lines, licence)
+            return False
     except PermissionError:
         sys.stdout.write(f"file {file} unreachable \n")
         return False
-    if len(lines) < 10:
-        return False
-    if (lines[0].find(APACHE_LICENCE[0]) != -1) or (lines[1].find(APACHE_LICENCE[0]) != -1):
-        if (lines[10].find(APACHE_LICENCE[10]) != -1) \
-                or (lines[11].find(APACHE_LICENCE[10]) != -1):
-            return True
-    if fix:
-        update_files(file, lines)
-    return False
 
 
-def read_files(fix: bool, root: str = "") -> list:
+def read_files(fixed: int, skipped: int, fix: bool, root: str = "") -> tuple[int, int]:
     root_path = Path(root)
-    result = []
     for path in os.listdir(root_path):
+        licence = None
         path = os.path.join(root_path, path)
         if os.path.isdir(path):
-            result.extend(read_files(fix, path))
-        if os.path.isfile(path) and (
-                path.endswith(".py") or path.endswith(".go") or path.endswith("go.mod")
-        ):
-            if not check_licence(path, fix):
-                result.append(path)
-    return result
+            fixed, skipped = read_files(fixed, skipped, fix, path)
+        if os.path.isfile(path):
+            if path.endswith(".py"):
+                licence = APACHE_LICENCE_PY
+            elif path.endswith(".go") or path.endswith("go.mod"):
+                licence = APACHE_LICENCE_GO
+            if licence:
+                if not check_licence(path, fix, licence):
+                    sys.stdout.write(f"{path} has no license\n")
+                    if fix:
+                        fixed += 1
+                    else:
+                        skipped += 1
+    return fixed, skipped
 
 
-def update_files(file: str, lines: list):
-    new_line = "\n"
-    separator = "# " if file.endswith(".py") else "// "
-    with open(file, "w", encoding="utf-8") as f:
-        lines.insert(0, f'{separator}{f"{new_line}{separator}".join(APACHE_LICENCE)}{new_line}')
-        f.writelines(lines)
+def update_files(f: TextIO, lines: list, licence: list[str]):
+    lines.insert(0, "".join(licence))
+    f.seek(0)
+    f.writelines(lines)
 
 
 if __name__ == "__main__":
@@ -77,16 +101,16 @@ if __name__ == "__main__":
                         help="Flag to fix absent license in file (default will only find it)")
     parser.add_argument("--folders", nargs="+", help="Folders to check")
     args = parser.parse_args()
-    empty_licence = []
+    number_of_fixed = number_of_skipped = 0
     for folder in args.folders:
-        empty_licence.extend(read_files(args.fix, folder))
-    if empty_licence:
-        sys.stdout.write(
-            f"Licence not detected in the following files: {', '.join(empty_licence)} \n"
+        number_of_fixed, number_of_skipped = read_files(
+            number_of_fixed, number_of_skipped, args.fix, folder
         )
-        if args.fix:
-            sys.stdout.write(f"{len(empty_licence)} was updated with the licence. \n")
-        sys.exit(1)
-    else:
+    if number_of_fixed == number_of_skipped == 0:
         sys.stdout.write("Licence is present in all python and go files \n")
         sys.exit(0)
+    sys.stdout.write(
+        f"Updating licence skipped in {number_of_skipped} files." \
+        f" Licence was updated in {number_of_fixed} files \n"
+    )
+    sys.exit(1)
