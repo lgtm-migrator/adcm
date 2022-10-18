24697db6e345d6fe829c06fb3be685a98f9a18d9
dskrynnik <danbalalan@gmail.com>
2022-10-18 18:25:32 +0300

    Revert "ADCM-3067 update issues on task finish"
    This reverts commit 9b03b764f614204cacbe16b6f906f8743f8dc379.

--- python/cm/models.py
+++ python/cm/models.py
@@ -1298,9 +1298,6 @@ JOB_STATUS = (
 )
 
 
-FINAL_JOB_STATUSES = ("success", "failed")
-
-
 class UserProfile(ADCMModel):
     login = models.CharField(max_length=32, unique=True)
     profile = models.JSONField(default=str)
