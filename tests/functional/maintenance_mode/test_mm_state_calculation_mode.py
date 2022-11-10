699bf889c32a71c88ab1e3fc987aa3fcfdf368a9
vchudasov <v.chudasov@arenadata.io>
2022-11-10 21:54:05 +0300

    ADCM-3261 add test MM with actions

--- tests/functional/maintenance_mode/test_mm_state_calculation_mode.py
+++ tests/functional/maintenance_mode/test_mm_state_calculation_mode.py
@@ -17,7 +17,7 @@ Test designed to check MM state calculation logic for services/components
 import allure
 import pytest
 
-from adcm_client.objects import Cluster
+from adcm_client.objects import Cluster, ADCMClient
 from tests.functional.conftest import only_clean_adcm
 from tests.functional.maintenance_mode.conftest import (
     ANOTHER_SERVICE_NAME,
@@ -26,17 +26,30 @@ from tests.functional.maintenance_mode.conftest import (
     MM_IS_ON,
     add_hosts_to_cluster,
     check_mm_is,
-    set_maintenance_mode, BUNDLES_DIR,
+    set_maintenance_mode, BUNDLES_DIR, get_enabled_actions_names, get_disabled_actions_names,
 )
+from tests.library.assertions import sets_are_equal
 
 
 # pylint: disable=redefined-outer-name
 
 @pytest.fixture()
-def cluster_with_issue_on_service_component(sdk_client_fs) -> Cluster:
+def cluster_with_mm(sdk_client_fs: ADCMClient) -> Cluster:
+    """
+    Upload cluster bundle with allowed MM,
+    create and return cluster with default service
+    """
+    bundle = sdk_client_fs.upload_from_fs(BUNDLES_DIR / 'cluster_mm_allowed')
+    cluster = bundle.cluster_create("Cluster with mm")
+    cluster.service_add(name="first_service")
+    return cluster
+
+
+@pytest.fixture()
+def cluster_with_action_on_service_component(sdk_client_fs: ADCMClient) -> Cluster:
     """Create cluster and add service"""
-    bundle = sdk_client_fs.upload_from_fs(BUNDLES_DIR / "cluster_with_issues" / "issue_service_component")
-    cluster = bundle.cluster_create("Cluster issues service component")
+    bundle = sdk_client_fs.upload_from_fs(BUNDLES_DIR / "cluster_service_component_actions")
+    cluster = bundle.cluster_create("Cluster actions")
     cluster.service_add(name="first_service")
     return cluster
 
@@ -141,7 +154,7 @@ def test_mm_state_host(api_client, cluster_with_mm, hosts):
 def test_mm_state_with_issue(api_client, cluster_with_issue_on_service_component):
     """
     Test to check maintenance_mode on components with issue
-    Issue on first_service and second_component
+    first_service and second_component have issue
     """
     first_service = cluster_with_issue_on_service_component.service(name="first_service")
     first_component = first_service.component(name="first_component")
@@ -157,7 +170,7 @@ def test_mm_state_with_issue(api_client, cluster_with_issue_on_service_component
 
         set_maintenance_mode(api_client=api_client, adcm_object=first_component, maintenance_mode=MM_IS_OFF)
         set_maintenance_mode(api_client=api_client, adcm_object=second_component, maintenance_mode=MM_IS_OFF)
-        check_mm_is(MM_IS_OFF, first_component, first_service)
+        check_mm_is(MM_IS_OFF, first_service, first_component, second_component)
 
     with allure.step('Check MM state calculation logic for service with issue'):
         set_maintenance_mode(api_client=api_client, adcm_object=first_service, maintenance_mode=MM_IS_ON)
@@ -167,4 +180,50 @@ def test_mm_state_with_issue(api_client, cluster_with_issue_on_service_component
         set_maintenance_mode(api_client=api_client, adcm_object=first_service, maintenance_mode=MM_IS_OFF)
         check_mm_is(MM_IS_OFF, first_service, first_component, second_component)
 
-    # TODO: add check with hosts issue, and add check that service/component in MM 'ON' turn to MM 'ON'
+
+def test_mm_state_with_action(api_client, cluster_with_action_on_service_component, hosts):
+    """
+    Test to check maintenance_mode on components with issue
+    first_service and second_component have issue
+    """
+    expected_enabled = {'default_action'} | {
+        f'{obj_type}_action_allowed' for obj_type in ('cluster', 'service', 'component')
+    }
+    expected_disabled = {f'{obj_type}_action_disallowed' for obj_type in ('cluster', 'service', 'component')}
+
+    host_in_mm, regular_host, *_ = hosts
+    cluster = cluster_with_action_on_service_component
+    service = cluster.service()
+    component = service.component()
+
+    add_hosts_to_cluster(cluster, (host_in_mm, regular_host))
+    cluster.hostcomponent_set((host_in_mm, component), (regular_host, component))
+
+    with allure.step('Check MM state calculation logic for service with action'):
+        set_maintenance_mode(api_client=api_client, adcm_object=service, maintenance_mode=MM_IS_ON)
+        check_mm_is(MM_IS_ON, service, component)
+        check_mm_is(MM_IS_OFF, host_in_mm, regular_host)
+
+    with allure.step('Check MM state calculation logic for component with action'):
+        set_maintenance_mode(api_client=api_client, adcm_object=component, maintenance_mode=MM_IS_ON)
+        check_mm_is(MM_IS_ON, service, component)
+        check_mm_is(MM_IS_OFF, host_in_mm, regular_host)
+
+    with allure.step("Switch service and component to MM 'OFF' and check logic"):
+        set_maintenance_mode(api_client=api_client, adcm_object=service, maintenance_mode=MM_IS_OFF)
+        set_maintenance_mode(api_client=api_client, adcm_object=component, maintenance_mode=MM_IS_OFF)
+        check_mm_is(MM_IS_OFF, service, component, host_in_mm, regular_host)
+
+    with allure.step("Switch host to MM 'ON' and check actions"):
+        set_maintenance_mode(api_client=api_client, adcm_object=host_in_mm, maintenance_mode=MM_IS_ON)
+        check_mm_is(MM_IS_ON, host_in_mm)
+        check_mm_is(MM_IS_OFF, service, component, regular_host)
+
+        enabled_actions = get_enabled_actions_names(regular_host)
+        disabled_actions = get_disabled_actions_names(regular_host)
+
+    with allure.step('Check that correct actions are enabled/disabled on the host'):
+        sets_are_equal(enabled_actions, expected_enabled, f'Incorrect actions are enabled on host {regular_host.fqdn}')
+        sets_are_equal(
+            disabled_actions, expected_disabled, f'Incorrect actions are disabled on host {regular_host.fqdn}'
+        )
