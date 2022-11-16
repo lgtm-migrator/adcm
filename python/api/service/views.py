25dd5cda77a6418f410699ff5b5e0ac2d58f8419
Konstantin Shestakov <k.shestakov@arenadata.io>
2022-11-16 20:24:06 +0700

    ADCM-3393 prohibit bound service deletion

--- python/api/service/views.py
+++ python/api/service/views.py
@@ -133,6 +133,9 @@ class ServiceDetailView(PermissionListMixin, DetailView):
         if instance.prototype.required:
             raise_adcm_ex("SERVICE_CONFLICT", f"Service #{instance.id} is required")
 
+        if ClusterBind.objects.filter(service=instance).exists():
+            raise_adcm_ex("SERVICE_CONFLICT", f"Service #{instance.id} has bind")
+
         delete_action = Action.objects.filter(
             prototype=instance.prototype, name=settings.ADCM_DELETE_SERVICE_ACTION_NAME
         ).first()
