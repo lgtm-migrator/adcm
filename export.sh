37df283039a58533146eaca735f71d902bf87f19
Konstantin Shestakov <k.shestakov@arenadata.io>
2022-11-17 16:01:17 +0700

    ADCM-3341 add docker compose and postgres service

---
+++ export.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export DATA_DIR="/home/winmasta/projects/arenadata/adcm/data"
+export POSTGRES_PASSWORD="hFyr4}J8tIKVORhRW9xx"
+export PGDATA="/home/winmasta/projects/arenadata/adcm/db"
