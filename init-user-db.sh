63d38a75a4b261ea100045123a6135c34eee7ca1
Konstantin Shestakov <k.shestakov@arenadata.io>
2022-11-19 12:57:27 +0700

    ADCM-3371 add ability to migrate to PostgreSQL

--- init-user-db.sh
+++ init-user-db.sh
@@ -3,6 +3,5 @@
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	CREATE USER adcm with encrypted password '$POSTGRES_ADCM_PASS';
 	CREATE DATABASE adcm;
-	GRANT ALL ON DATABASE adcm TO adcm;
 	ALTER DATABASE adcm OWNER TO adcm;
 EOSQL
