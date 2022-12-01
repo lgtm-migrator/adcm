b36aff2b244bf862c4e4bcf5b3218f9ae021e85a
Daniil S <d.skrynnik@arenadata.io>
2022-12-01 10:43:21 +0300

    ADCM-3395 aborted action state/multistate

--- python/cm/job.py
+++ python/cm/job.py
@@ -750,7 +750,7 @@ def get_state(action: Action, job: JobLog, status: str) -> Tuple[Optional[str], 
         state = action.state_on_success
         if not state:
             logger.warning('action "%s" success state is not set', action.name)
-    elif status == JobStatus.FAILED:
+    elif status in (JobStatus.FAILED, JobStatus.ABORTED):
         state = getattr_first("state_on_fail", sub_action, action)
         multi_state_set = getattr_first("multi_state_on_fail_set", sub_action, action)
         multi_state_unset = getattr_first("multi_state_on_fail_unset", sub_action, action)
